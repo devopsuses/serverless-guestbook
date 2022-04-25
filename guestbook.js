97082791dfdba438bdd9443d9fe6c8bf76602f80
devopsuses <78223770+devopsuses@users.noreply.github.com>
2022-04-25 14:19:13 +0000

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://a9ad4725.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
